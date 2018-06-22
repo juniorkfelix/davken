f2fdf1473746b63d72a4442f0a290e82b8fdc798
juniorkfelix <felixonsare@gmail.com>
2018-06-22 09:57:45 +0300

    few changes

--- siteapp/views.py
+++ siteapp/views.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+
+def about(request):
+	return render(request, 'about.html')
