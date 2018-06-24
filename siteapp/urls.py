afdba136b11e1dca075d37dbcca73813677ebe36
juniorkfelix <felixonsare@gmail.com>
2018-06-24 12:35:08 +0300

    several modifications to all pages

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index , name="index"),
 	url(r'^index', views.index),
-	url(r'^about/$', views.about, name="about"),
+	url(r'^about$', views.about, name="about"),
+	url(r'^categories$', views.categories, name="categories"),
+	url(r'^contact$', views.contact, name="contact"),
+	url(r'^form$', views.get_name, name="form"),
+
 ]
--- siteapp/views.py
+++ siteapp/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from . forms import NameForm
 
 # Create your views here.
 def index(request):
@@ -10,3 +11,21 @@ def index(request):
 
 def about(request):
 	return render(request, 'about.html')
+
+def categories(request):
+	return render(request, 'categories.html')
+
+def contact(request):
+	return render(request, 'contact.html')
+
+def get_name(request):
+	if request.method == 'POST':
+
+		form = NameForm(request.POST)
+		if form.is_valid():
+			return HttpResponseRedirect('/thanks/')
+
+	else:
+		form = NameForm()
+
+	return render(request, 'form.html', {'form':form})
