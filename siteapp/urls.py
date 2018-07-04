f6b9bc771125177e597c0478d3ae42fe171ed403
juniorkfelix <felixonsare@gmail.com>
2018-07-04 18:54:06 +0300

    added login panel to te webite

--- siteapp/urls.py
+++ siteapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.auth.views import login
 
 urlpatterns = [
 	url(r'^$', views.index , name="index"),
@@ -7,6 +8,6 @@ urlpatterns = [
 	url(r'^about$', views.about, name="about"),
 	url(r'^categories$', views.categories, name="categories"),
 	url(r'^contact$', views.contact, name="contact"),
-	
+	url(r'^login/$', login, {'template_name': 'login.html'})
 
 ]
